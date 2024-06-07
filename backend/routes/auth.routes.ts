2949ab4e4e1572e4a81d0cd69c6f3ab465d51fa4
SarweshKumarTiwari <sarweshtiwari14@gmail.com>
2024-06-07 15:21:02 +0530

    added routes  finally

---
+++ backend/routes/auth.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import {
+    authenticate,
+    authorise,
+    getLoggedUser,
+    logoutUser,
+    registerUser,
+} from "../controllers/user.controller";
+
+const authRoute = Router();
+
+authRoute.post('/user/register', registerUser);
+authRoute.post('/user/authorise', authorise);
+authRoute.get('/user/userInfo', authenticate, getLoggedUser);
+authRoute.get('/user/logout',authenticate,logoutUser)
+
+export default authRoute;
